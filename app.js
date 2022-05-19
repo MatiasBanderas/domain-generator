eba2f936109950c18ecb50fba846ec73c361adc4
MatiasBanderas <matias.banderas@ug.uchile.cl>
2022-05-19 05:37:25 +0000

    tarea realizada

---
+++ app.js
@@ -0,0 +1,10 @@
+
+
+
+let uno = ['the','our'];
+let dos = ['great', 'big' ];
+let tres = ['jogger','racoon'];
+
+let response = uno[Math.floor(Math.random() * uno.length)] + dos[Math.floor(Math.random() * dos.length)] + tres[Math.floor(Math.random() * tres.length)] + ".com"
+
+console.log(response)
